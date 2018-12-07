f646ebdf92babe65aa940e483fde4ce353c0cc29
Tom Duncalf <tom@tomduncalf.com>
2018-12-07 09:26:06 +0000

    Add time range and limit filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Table, TableRow, TableBody, TableCell, Grid } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
+import { Table, TableRow, TableBody, TableCell, Grid, Select, Button, FormControl, InputLabel, MenuItem } from "@material-ui/core";
 import * as React from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 
 const initialState = {
   authenticated: false,
+  limit: 20,
+  timeRange: 'medium_term',
   topArtists: undefined as SpotifyApi.UsersTopArtistsResponse | undefined,
-  topTracks: undefined as SpotifyApi.UsersTopTracksResponse | undefined
+  topTracks: undefined as SpotifyApi.UsersTopTracksResponse | undefined,
 };
 
 class App extends React.Component<{}, typeof initialState> {
@@ -43,12 +44,13 @@ class App extends React.Component<{}, typeof initialState> {
   getData = async () => {
     try {
       const [topArtists, topTracks] = await Promise.all([
-        this.spotifyApi.getMyTopArtists(),
-        this.spotifyApi.getMyTopTracks()
+        this.spotifyApi.getMyTopArtists({ time_range: this.state.timeRange, limit: this.state.limit }),
+        this.spotifyApi.getMyTopTracks({ time_range: this.state.timeRange, limit: this.state.limit }),
       ]);
 
-      this.setState({ topArtists, topTracks }); // tslint:disable-line
+      this.setState({ topArtists, topTracks });
     } catch (e) {
+      console.error(e); //tslint:disable-line
       this.setState({ authenticated: false });
     }
   };
@@ -56,6 +58,51 @@ class App extends React.Component<{}, typeof initialState> {
   searchBandcamp = (query: string) => {
     window.open("https://bandcamp.com/search?q=" + encodeURIComponent(query));
   };
+
+  renderHeader = () => (
+    <Grid item xs={12}>
+      <FormControl>
+        <InputLabel htmlFor="time-range">Time range</InputLabel>
+        <Select
+          value={this.state.timeRange}
+          onChange={this.handleTimeRangeChange}
+          inputProps={{
+            name: 'time-range',
+          }}
+          autoWidth
+        >
+          <MenuItem value='short_term'>Short term</MenuItem>
+          <MenuItem value='medium_term'>Medium term</MenuItem>
+          <MenuItem value='long_term'>Long term</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl>
+        <InputLabel htmlFor="limit">Items</InputLabel>
+        <Select
+          value={this.state.limit}
+          onChange={this.handleLimitChange}
+          inputProps={{
+            name: 'limit',
+          }}
+          autoWidth
+        >
+          <MenuItem value='10'>10</MenuItem>
+          <MenuItem value='20'>20</MenuItem>
+          <MenuItem value='35'>35</MenuItem>
+          <MenuItem value='50'>50</MenuItem>
+        </Select>
+      </FormControl>
+    </Grid>
+  );
+
+  handleTimeRangeChange = (timeRange: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ timeRange: timeRange.target.value }, () => this.getData());
+  }
+
+  handleLimitChange = (limit: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ limit: parseInt(limit.target.value) }, () => this.getData());
+  }
 
   renderTopArtists = () => {
     const { topArtists } = this.state;
@@ -134,6 +181,7 @@ class App extends React.Component<{}, typeof initialState> {
 
         {authenticated ? (
           <Grid container spacing={24}>
+            {this.renderHeader()}
             {this.renderTopArtists()}
             {this.renderTopTracks()}
           </Grid>
@@ -146,6 +194,8 @@ class App extends React.Component<{}, typeof initialState> {
             Authenticate with Spotify
           </Button>
         )}
+
+        <p>Built by <a href="https://twitter.com/tomduncalf">Tom Duncalf</a>. Source code available on <a href="https://github.com/tomduncalf/supportify">Github</a>.</p>
       </React.Fragment>
     );
   }
